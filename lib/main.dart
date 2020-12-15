a09bcb559449fae8d10909691980c37e260ef6ec
Varun S Athreya <varun.sathreya@gmail.com>
2020-12-15 23:07:01 +0530

    Removed debug tag and made visualDensity adaptive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GfG JSSATEB',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.white,
         accentColor: const Color(0xFF2F8D46),
         backgroundColor: const Color(0xFFEEEEEE),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
         appBar: AppBar(
