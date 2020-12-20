8154513eb5da567299c8dbbad66a2bf27132a8ad
Varun S Athreya <varun.sathreya@gmail.com>
2020-12-21 01:00:52 +0530

    Added addAnnouncement function

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/uuid_util.dart';
 
 import '../models/announcement.dart';
 import '../models/student.dart';
 
 class DatabaseService {
   DatabaseService._();
+  static Uuid uuid = Uuid(options: {'grng': UuidUtil.cryptoRNG});
 
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
@@ -48,5 +51,16 @@ class DatabaseService {
       announcements.add(Announcement.fromDocumentSnapshot(docSnapshot));
     }
     return announcements;
+  }
+
+  static Future<void> addAnnouncement(
+      {String title, String description}) async {
+    final String id = uuid.v4();
+    await _firestore.collection('announcements').doc(id).set({
+      'id': id,
+      'title': title,
+      'description': description,
+      'timestamp': Timestamp.now(),
+    });
   }
 }
