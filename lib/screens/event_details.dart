61a4b0532d57792e530a08c9dd2e4b5a307f0ef1
Varun S Athreya <varun.sathreya@gmail.com>
2020-12-29 01:16:17 +0530

    Fixed error detail null error

--- lib/screens/event_details.dart
+++ lib/screens/event_details.dart
@@ -11,6 +11,7 @@ import '../models/student_data.dart';
 import '../services/database.dart';
 import '../widgets/alert_dialog.dart';
 import '../widgets/custom_appbar.dart';
+import '../widgets/error_message.dart';
 import 'admin/add_events.dart';
 
 class EventDetails extends StatefulWidget {
@@ -90,143 +91,168 @@ class _EventDetailsState extends State<EventDetails> {
             ),
             child: SingleChildScrollView(
               child: StreamBuilder<DocumentSnapshot>(
-                  stream: DatabaseService.getEvent(id: widget.id),
-                  builder: (context, snapshot) {
-                    final Event event =
-                        Event.fromDocumentSnapshot(snapshot?.data);
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          event.title,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.center,
-                          textScaleFactor: 2,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).accentColor,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Text(
-                          'Event On: ${dateFormat.format(event.date)}',
-                          textScaleFactor: 1.3,
-                          maxLines: 2,
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(
-                            color: Theme.of(context).accentColor,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          'Register By: ${registerFormat.format(event.register)}',
-                          textScaleFactor: 1.3,
-                          maxLines: 2,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Theme.of(context).accentColor,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Text(
-                          event.description,
-                          softWrap: true,
-                          textAlign: TextAlign.center,
-                          textScaleFactor: 1.2,
-                        ),
-                        const SizedBox(height: 20),
-                        if (!event.participants.contains(uid))
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              MaterialButton(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.06,
-                                minWidth:
-                                    MediaQuery.of(context).size.width * 0.6,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20)),
-                                onPressed: () async {
-                                  try {
-                                    setState(() {
-                                      _isLoading = true;
-                                    });
-                                    await DatabaseService.registerToEvent(
-                                      uid: uid,
-                                      email: email,
-                                      eventId: event.id,
-                                    );
-
-                                    _scaffoldKey.currentState
-                                        .showSnackBar(const SnackBar(
-                                      content: Text('Registerd Successfully'),
-                                    ));
-                                  } catch (e) {
-                                    _scaffoldKey.currentState.showSnackBar(
-                                        SnackBar(content: Text('$e')));
-                                  } finally {
-                                    setState(() {
-                                      _isLoading = false;
-                                    });
-                                  }
-                                },
-                                color: Theme.of(context).accentColor,
-                                child: const Text(
-                                  "Register",
-                                  textScaleFactor: 1.4,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                        if (isAdmin) ...[
-                          const SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              IconButton(
-                                icon: Icon(
-                                  FontAwesomeIcons.pen,
-                                  color: Theme.of(context).accentColor,
-                                ),
-                                onPressed: () => Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => AddEvent(
-                                      event: event,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              IconButton(
-                                icon: Icon(
-                                  FontAwesomeIcons.trash,
-                                  color: Theme.of(context).accentColor,
-                                ),
-                                onPressed: () => customAlertDialog(
-                                  context: context,
-                                  title: 'Deleting Event!!!',
-                                  description:
-                                      'Are you sure you want to delete this event!?',
-                                  onOK: () {
-                                    DatabaseService.deleteEvent(id: event.id);
-                                    Navigator.pop(context);
-                                    Navigator.pop(context);
-                                  },
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(height: 10)
-                        ],
-                      ],
+                stream: DatabaseService.getEvent(id: widget.id),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: ErrorMessage(
+                        message: snapshot.error,
+                      ),
                     );
-                  }),
+                  } else if (snapshot.hasData) {
+                    final Event event =
+                        Event.fromDocumentSnapshot(snapshot.data);
+                    return buildEventDetail(
+                      event: event,
+                      context: context,
+                      uid: uid,
+                      email: email,
+                      isAdmin: isAdmin,
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Column buildEventDetail({
+    @required Event event,
+    @required BuildContext context,
+    @required String uid,
+    @required String email,
+    @required bool isAdmin,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          event.title,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+          textScaleFactor: 2,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'Event On: ${dateFormat.format(event.date)}',
+          textScaleFactor: 1.3,
+          maxLines: 2,
+          textAlign: TextAlign.justify,
+          style: TextStyle(
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          'Register By: ${registerFormat.format(event.register)}',
+          textScaleFactor: 1.3,
+          maxLines: 2,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          event.description,
+          softWrap: true,
+          textAlign: TextAlign.center,
+          textScaleFactor: 1.2,
+        ),
+        const SizedBox(height: 20),
+        if (!event.participants.contains(uid))
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                height: MediaQuery.of(context).size.height * 0.06,
+                minWidth: MediaQuery.of(context).size.width * 0.6,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () async {
+                  try {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                    await DatabaseService.registerToEvent(
+                      uid: uid,
+                      email: email,
+                      eventId: event.id,
+                    );
+
+                    _scaffoldKey.currentState.showSnackBar(const SnackBar(
+                      content: Text('Registerd Successfully'),
+                    ));
+                  } catch (e) {
+                    _scaffoldKey.currentState
+                        .showSnackBar(SnackBar(content: Text('$e')));
+                  } finally {
+                    setState(() {
+                      _isLoading = false;
+                    });
+                  }
+                },
+                color: Theme.of(context).accentColor,
+                child: const Text(
+                  "Register",
+                  textScaleFactor: 1.4,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        if (isAdmin) ...[
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(
+                  FontAwesomeIcons.pen,
+                  color: Theme.of(context).accentColor,
+                ),
+                onPressed: () => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddEvent(
+                      event: event,
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Theme.of(context).accentColor,
+                ),
+                onPressed: () => customAlertDialog(
+                  context: context,
+                  title: 'Deleting Event!!!',
+                  description: 'Are you sure you want to delete this event!?',
+                  onOK: () {
+                    DatabaseService.deleteEvent(id: event.id);
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 10)
+        ],
+      ],
     );
   }
 }
