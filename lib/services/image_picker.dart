8bf8e0bc1099834605f11e28136bfbcd7ef78ace
Varun S Athreya <varun.sathreya@gmail.com>
2020-12-28 21:05:06 +0530

    Added image quality for picked image

--- lib/services/image_picker.dart
+++ lib/services/image_picker.dart
@@ -7,7 +7,8 @@ class ImagePickerService {
   final imagePicker = ImagePicker();
   // Returns a [File] object pointing to the image that was picked.
   Future<File> pickImage({@required ImageSource source}) async {
-    final pickedFile = await imagePicker.getImage(source: source);
+    final pickedFile =
+        await imagePicker.getImage(source: source, imageQuality: 75);
     return File(pickedFile.path);
   }
 }
