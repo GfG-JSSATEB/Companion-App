2525d49ee8a9d4e472c30995d22313b4389bec1b
Varun S Athreya <varun.sathreya@gmail.com>
2020-12-28 21:05:39 +0530

    Added storage service to store event posters

---
+++ lib/services/storage.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+
+class StorageService {
+  StorageService({@required this.id}) : assert(id != null);
+  final String id;
+
+  Future<String> uploadPoster({
+    @required File file,
+  }) async {
+    return await upload(
+      file: file,
+      path: 'event/$id.png',
+      contentType: 'image/png',
+    );
+  }
+
+  Future<String> upload({
+    @required File file,
+    @required String path,
+    @required String contentType,
+  }) async {
+    final storageReference = FirebaseStorage.instance.ref().child(path);
+    final UploadTask uploadTask = storageReference.putFile(
+        file, SettableMetadata(contentType: contentType));
+
+    final TaskSnapshot snapshot = await uploadTask;
+
+    final String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+}
