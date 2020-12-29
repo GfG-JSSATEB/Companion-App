3b880fca7678856a91cf589a999eea0e9ce725e7
Varun S Athreya <varun.sathreya@gmail.com>
2020-12-30 00:07:54 +0530

    Added cloud function when announcement is created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const fcm = admin.messaging();
+
+export const sendNotification = functions.firestore
+    .document('announcements/{announcementID}')
+    .onCreate(async (snapshot) => {
+        const announcement = snapshot.data();
+        const condition = "!('industry-tech' in topics)";
+        const payload: admin.messaging.MessagingPayload = {
+            notification: {
+                title: 'New Announcement!',
+                body: announcement.title,
+                clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+                icon:
+                    'https://drive.google.com/uc?export=view&id=1uJN7SufcHG4eHscBSbfdowyVlychy407',
+            },
+        };
+
+        return fcm.sendToCondition(condition, payload);
+    });
